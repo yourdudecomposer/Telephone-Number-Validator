22e8f041c0f7f9f90a936ac0dad7c36e16bdcbd5
Filipp Goriainov <barrios1991phil@gmail.com>
2022-01-31 14:55:03 +0400

    make simple reg

--- functions.js
+++ functions.js
@@ -1,8 +1,10 @@
-// const rePop = /^(1 |1)*(\d{3}|\(\d{3}\))[ -]{0,1}(\d{3})[ -]{0,1}(\d{4}$)/g;
+// const rePop = /^(1 |1)*(\d{3}|\(\d{3}\))[ -]{0,1}(\d{3})[ -]{0,1}(\d{4}$)/;
+// const reFil = /^1?\s?(\d{3}|\(\d{3}\))[-\s]?\d{3}[-\s]?\d{4}$/;
+const reSimple = /^(1 |1|)(\d{3}|\(\d{3}\))( ||-)(\d{3})( ||-)(\d{4}$)/;
 
-const reFil = /^1?\s?(\d{3}|\(\d{3}\))[-\s]?\d{3}[-\s]?\d{4}$/;
 
 function telephoneCheck(str) {
-
-    return reFil.test(str);
+    return reSimple.test(str);
 }
+
+
