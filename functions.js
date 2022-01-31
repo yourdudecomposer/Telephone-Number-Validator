6d0a06c4f1a31c9f9c9187e5a0357da79a4c0d16
Filipp Goriainov <filippgoriainov@Air-Filipp.Dlink>
2022-01-31 13:58:53 +0400

    chek for contributions

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 const rePop = /^(1 |1)*(\d{3}|\(\d{3}\))[ -]{0,1}(\d{3})[ -]{0,1}(\d{4}$)/g;
 
-//const reFil =
+//const reFil = /^1?\s?(\d{3}|\(\d{3}\))[-\s]?\d{3}[-\s]?\d{4}$/;
 
 function telephoneCheck(str) {
 
